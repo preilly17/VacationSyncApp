625b1e5d4fff2e82d7e4d4d52657bdf6d1b352be
Patch Reilly <patch.reilly1@gmail.com>
2026-01-11 13:06:21 -0500

    Initial Commit

---
+++ TripSyncApp/TripSyncAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripSyncAppApp.swift
+//  TripSyncApp
+//
+//  Created by Patrick Reilly on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct TripSyncAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
